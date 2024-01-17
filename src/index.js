629c7bee4536921b85e8b4b84628403ed839dea7
Francesco Cecca <fcecca3@gmail.com>
2024-01-17 17:43:04 +0100

    test

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import './index.css';
+import App from './App';
+import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { store } from './redux/store';
+
+const root = document.getElementById('root');
+const reactRoot = createRoot(root);
+
+reactRoot.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
